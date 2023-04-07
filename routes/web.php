f3a1a3bd06fadf85541a93f8d38db86b7d47f54f
user <nikacuckiridze47@gmail.com>
2023-04-07 18:00:29 +0400

    refactor: group routes with prefix and controller

--- routes/web.php
+++ routes/web.php
@@ -24,30 +24,38 @@ Route::group(['middleware' => "localization"], function () {
 
 
     Route::group(['middleware' => "guest"], function () {
-        Route::view('/login', "auth.login")->name('auth.login');
-        Route::post('/login', [AuthController::class, "login"])->name('auth.login');
-
+        Route::group(['prefix' => "login"], function () {
+            Route::view('/', 'auth.login')->name('auth.google');
+            Route::post('/', [AuthController::class, "login"])->name('auth.login');
+        });
         Route::view('/register', "auth.register")->name('auth.register');
     });
 
     Route::group(['middleware' => "auth"], function () {
         Route::get('/logout', [AuthController::class, "logout"])->name('auth.logout');
 
-        Route::get('/dashboard', [AuthController::class, "dashboard"])->name('dashboard');
-        Route::get('/dashboard/quotes', [AuthController::class, "quotes"])->name('dashboard.quotes');
-        Route::get('/dashboard/movies', [AuthController::class, "movies"])->name('dashboard.movies');
+        Route::group(['prefix' => "dashboard", 'controller' => AuthController::class], function () {
+            Route::get('', "dashboard")->name('dashboard');
+            Route::get('/quotes', "quotes")->name('dashboard.quotes');
+            Route::get('/movies', "movies")->name('dashboard.movies');
+        });
 
-        Route::view('/movies/create', 'movies.create')->name('movies.create');
-        Route::post('/movies/store', [MovieController::class, 'store'])->name('movies.store');
-        Route::get('/movies/edit/{movie}', [MovieController::class, 'edit'])->name('movies.edit');
-        Route::patch('/movies/update/{movie}', [MovieController::class, 'update'])->name('movies.update');
-        Route::delete('/movies/destroy/{movie}', [MovieController::class, 'destroy'])->name('movies.destroy');
+        Route::group(['prefix' => "movies", 'controller' => MovieController::class], function () {
+            Route::view('/create', 'movies.create')->name('movies.create');
+            Route::post('/store', 'store')->name('movies.store');
+            Route::get('/edit/{movie}', 'edit')->name('movies.edit');
+            Route::patch('/update/{movie}', 'update')->name('movies.update');
+            Route::delete('/destroy/{movie}',  'destroy')->name('movies.destroy');
+        });
 
         Route::get('/movies/{movie:slug}/quotes/create', [QuoteController::class, 'create'])->name('quotes.create');
-        Route::post('/quotes/store', [QuoteController::class, "store"])->name('quotes.store');
-        Route::get('/quotes/edit/{quote}', [QuoteController::class, "edit"])->name('quotes.edit');
-        Route::patch('/quotes/update/{quote}', [QuoteController::class, "update"])->name('quotes.update');
-        Route::delete('/quotes/delete/{quote}', [QuoteController::class, "destroy"])->name('quotes.destroy');
+
+        Route::group(['prefix' => 'quotes', 'controller' => QuoteController::class], function () {
+            Route::post('/store', 'store')->name('quotes.store');
+            Route::get('/edit/{quote}', 'edit')->name('quotes.edit');
+            Route::patch('/update/{quote}', 'update')->name('quotes.update');
+            Route::delete('/destroy/{quote}', 'destroy')->name('quotes.destroy');
+        });
     });
 
 
