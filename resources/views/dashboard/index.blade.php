e9c3fad39605c776c30feec0fa72393fedb48861
user <nikacuckiridze47@gmail.com>
2023-04-10 16:28:59 +0400

    fix: closes #11, #12, #13

--- resources/views/dashboard/index.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-dashboard-title/>
-    <div class="flex w-175 mx-auto justify-center gap-5">
-        <x-button class="text-2xl font-bold" href="{{ route('dashboard.movies') }}">{{ __('form.dashboard.movies') }}</x-button>
-        <x-button class="text-2xl font-bold" href="{{ route('dashboard.quotes') }}">{{ __('form.dashboard.quotes') }}</x-button>
-    </div>
-</x-layout>
