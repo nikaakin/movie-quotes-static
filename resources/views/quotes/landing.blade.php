40130e24b5cd67ad82d5b2524640a6dda045e527
user <nikacuckiridze47@gmail.com>
2023-04-10 15:48:43 +0400

    fix: add image from factory

--- resources/views/quotes/landing.blade.php
+++ resources/views/quotes/landing.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="flex items-center mt-48 flex-col text-white text-4xl">
-        <img src="/storage/{{ $quote->photo }}" alt="alt" class=" w-175 h-auto mb-16">
+        <img src="{{asset('storage/photos').'/'. $quote->photo  }}" alt="alt" class=" w-175 h-auto mb-16">
         <p class="mb-28">“{{ $quote->getTranslation('quote', app()->getLocale()) }}”</p>
         <h1><a href="{{ route('movies.show',$quote->movie->slug  ) }}" class="underline">{{ $quote->movie->title }}</a></h1>
     </div>
